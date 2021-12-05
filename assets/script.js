b508551fadd6e4ccf5f08b754c4b47d429f2f26b
JohnPac <johnpac15@yahoo.com>
2021-12-05 15:46:54 -0600

    added base HTML, bootstrap, and general layout of web page

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+getWeather = function() {
+    let getThatWeater = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={d310cdc3e7de424fc0047cf1fd72fd27}";
+
+    fetch(getThatWeater)
+    
+    console.log(getThatWeater);
+}
+
+getWeather();
