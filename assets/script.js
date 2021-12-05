dd4a7ad3567ebfbf2cd616f0c3e98dd84f08ca56
JohnPac <johnpac15@yahoo.com>
2021-12-05 17:44:15 -0600

    fitch is working with hard code.

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,35 @@
-getWeather = function() {
-    let getThatWeater = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={d310cdc3e7de424fc0047cf1fd72fd27}";
+var city = document.getElementById("display-city")
+var currentTemp = document.getElementById("current-temp")
+var wind = document.getElementById("wind")
+var humidity = document.getElementById("humidity")
+var uvIndex = document.getElementById("uv-index")
 
-    fetch(getThatWeater)
+
+getWeather = function() {
+    let getThatWeater = "https://api.openweathermap.org/data/2.5/weather?id=1690295&units=imperial&appid=d310cdc3e7de424fc0047cf1fd72fd27";
+
+    fetch(getThatWeater).then(function(response){
+        
+        console.log(getThatWeater);
+        console.log(response);
+
+        if(response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                let tempNow = data.main.temp
+                let windSpeed = data.wind.speed
+                let humidtyNow = data.main.humidity
+
+                console.log(humidtyNow)
+                console.log(windSpeed)
+
+                city.textContent = data.name + " / " + tempNow;
+                wind.textContent = windSpeed
+                humidity.textContent = humidtyNow
+            })
+        }
+    })
     
-    console.log(getThatWeater);
 }
 
 getWeather();
