7b2de4ec75d56c83c29e03321244aa5536e6abb0
JohnPac <johnpac15@yahoo.com>
2021-12-06 13:53:51 -0600

    added deatils to weather blocks

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 cityArray = JSON.parse(localStorage.getItem("City History"))
 
-
+var theCityName = document.getElementById("display-city-name")
 var city = document.getElementById("display-city")
 var currentTemp = document.getElementById("current-temp")
 var wind = document.getElementById("wind")
@@ -23,23 +23,26 @@ getWeather = function(C) {
         if(response.ok) {
             response.json().then(function(data) {
                 console.log(data);
+                let cityMainName = data.name
                 let tempNow = data.main.temp
                 let windSpeed = data.wind.speed
                 let humidtyNow = data.main.humidity
                 let lat = data.coord.lat
                 let lon = data.coord.lon
                 
+                console.log(cityMainName)
                 console.log(tempNow)
                 console.log(humidtyNow)
                 console.log(humidtyNow)
                 console.log(lat)
                 console.log(lon)
                 
-                city.textContent = "Currently in " + data.name + " the temperature is: " + tempNow;
+                theCityName.textContent = cityMainName
+                city.textContent = "Current emperature is: " + tempNow;
                 wind.textContent= "Current windspeed is: " + windSpeed
                 humidity.textContent = "Current humitiy is: " + humidtyNow + "%"
                 
-                let getMoreWeather = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=d310cdc3e7de424fc0047cf1fd72fd27"
+                let getMoreWeather = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=d310cdc3e7de424fc0047cf1fd72fd27"
 
                 fetch(getMoreWeather).then(function(response) {
                     if(response.ok) {
@@ -56,6 +59,10 @@ getWeather = function(C) {
                                 let theDay = theDate.getDate()
                                 let theYear = theDate.getFullYear()
                                 let fullDate = theMonth + "/" + theDay + "/" +  theYear
+
+                                let forcastTemp= citydata.daily[i].temp.day
+                                let forcastWindSpeed = citydata.daily[i].wind_speed
+                                let forcastHumidity = citydata.daily[i].humidity
 
 
                                 console.log(fullDate);
@@ -74,8 +81,12 @@ getWeather = function(C) {
                                 divEl.appendChild(p3El)
                                 divEl.appendChild(p4El)
 
-                                divEl.setAttribute("style", "border: 3px solid black;")
+                                divEl.setAttribute("style", "border: 3px solid black; width: auto;")
                                 h4El.textContent = fullDate
+                                p2El.textContent = "Temperature: " + forcastTemp
+                                p3El.textContent = forcastWindSpeed + " MPH "
+                                p4El.textContent = " Humidity: " + forcastHumidity
+
 
                             }
                         })
